dae73d490b8fab55cfa6ade2f3fab551a9b4b4b6
Felixsynstein <tagulaofelix16@gmail.com>
2025-03-19 23:22:16 +0800

    initial commit

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
     $sql = "SELECT * FROM tasks ORDER BY created_at DESC";
     $result = $conn->query($sql);
-    $tasks = $result->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -16,21 +15,24 @@
 </head>
 
 <body>
-    <h1>To-Do List</h1>
-    <p>New Task</p>
+    
+    
     <div class="container">
-        <h2>New Task</h2>
-        <form action="add_task.php" method="POST">
-            <input type="text" name="task" placeholder="Task" required>
-            <button type="submit">Add Task</button>
-        </form>
-
+        <h1>Felix's To-Do List</h1><h2>New Task</h2>
+        <span class="add_task">
+            <form action="add_task.php" method="POST">
+                <input type="text" name="task" placeholder="Task" required>
+                <button type="submit">Add Task</button>
+            </form>
+        </span>
+        
         <h3>Task List</h3>
         <div class="task-container">
             <?php
                 $task = "SELECT * FROM tasks WHERE is_completed = 0 ORDER BY created_at DESC";
                 $task_result = $conn->query($task);
-                foreach ($task_result as $row){?>
+                foreach ($task_result as $row){
+            ?>
                     <div class="Task">
                         <span><?php echo htmlspecialchars($row['task_name']); ?></span>
                         <a href="complete_task.php?id=<?php echo $row['id']; ?>" class="button complete-btn">Complete</a>
@@ -42,14 +44,15 @@
         <h3>Completed Tasks</h3>
         <div class="task-container">
             <?php
-            $completed_sql = "SELECT * FROM tasks WHERE is_completed = 1 ORDER BY created_at DESC";
-            $completed_result = $conn->query($completed_sql);
-            foreach ($completed_result as $row){?>
-                <div class="task completed">
+                $completed_sql = "SELECT * FROM tasks WHERE is_completed = 1 ORDER BY created_at DESC";
+                $completed_result = $conn->query($completed_sql);
+                foreach ($completed_result as $row){
+            ?>
+                    <div class="task completed">
                     <span><?php echo htmlspecialchars($row['task_name']); ?></span>
                     <a href="delete_task.php?id=<?php echo $row['id']; ?>" class="button delete-btn">Delete</a>
-                </div>
-            <?php }; ?>
+                    </div>
+                <?php }; ?>
         </div>
     </div>
 </body>
